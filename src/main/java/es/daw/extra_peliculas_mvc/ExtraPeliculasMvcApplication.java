3f9185fb845d1f9f13c3677eafb655059e82c404
melola <melola@educa.madrid.org>
2026-04-28 20:52:42 +0200

    primer commit

---
+++ src/main/java/es/daw/extra_peliculas_mvc/ExtraPeliculasMvcApplication.java
@@ -0,0 +1,13 @@
+package es.daw.extra_peliculas_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraPeliculasMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtraPeliculasMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/daw/extra_peliculas_mvc/ExtraPeliculasMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw.extra_peliculas_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraPeliculasMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
